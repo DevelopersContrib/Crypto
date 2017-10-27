909fee525a9a46d000791fbb9cbf5f02caf1550e
rownone <row_none@yahoo.com>
2017-10-27 10:04:46 +0800

    changed token name

--- CTBAirdrop.sol
+++ CTBAirdrop.sol
@@ -24,21 +24,10 @@ contract Ownable {
       owner = newOwner;
     }
   }
-
 }
 
-contract CTBBasic {
-  uint public totalSupply;
-  function balanceOf(address who) constant public returns (uint);
-  function transfer(address to, uint value) public;
-  event Transfer(address indexed from, address indexed to, uint value);
-}
- 
-contract ERC20 is CTBBasic {
-  function allowance(address owner, address spender) constant public returns (uint);
-  function transferFrom(address from, address to, uint value) public;
-  function approve(address spender, uint value) public;
-  event Approval(address indexed owner, address indexed spender, uint value);
+contract CTBToken {
+	function transfer(address to, uint value) public;
 }
 
 contract CTBAirdropper is Ownable {
@@ -48,7 +37,7 @@ contract CTBAirdropper is Ownable {
     public returns (uint256) {
         uint256 i = 0;
         while (i < dests.length) {
-           ERC20(_tokenAddr).transfer(dests[i], values[i]);
+           CTBToken(_tokenAddr).transfer(dests[i], values[i]);
            i += 1;
         }
         return(i);
