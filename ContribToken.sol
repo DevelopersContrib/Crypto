b81785e3d6cd18c319863017494b03321edd78c9
rownone <row_none@yahoo.com>
2019-09-27 15:57:33 +0800

    pragma solidity >=0.4.22 <0.6.0;

--- ContribToken.sol
+++ ContribToken.sol
@@ -16,10 +16,8 @@ contract owned {
         owner = newOwner;
     }
 }
-
 						   
 interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes calldata _extraData) external; }
- 
 
 contract TokenERC20 {
     // Public variables of the token
