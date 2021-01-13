e990b57cc52b64bdee105ccf960278a0dababcfe
ronan <row_none@yahoo.com>
2021-01-13 14:46:33 +0800

    set 1B initial

--- RDAO/migrations/2_deploy_token.js
+++ RDAO/migrations/2_deploy_token.js
@@ -4,5 +4,5 @@ const RDAO = artifacts.require('RDAO');
 const { deployProxy } = require('@openzeppelin/truffle-upgrades');
  
 module.exports = async function (deployer) {
-  await deployProxy(RDAO, ['RDAO Token', 'RDAO', '100000000000000000000000'], { deployer, initializer: 'initialize' });
+  await deployProxy(RDAO, ['RDAO Token', 'RDAO', '1000000000000000000000000000'], { deployer, initializer: 'initialize' });
 };
